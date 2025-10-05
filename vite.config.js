6c93fb9eb1605608334975e363165689b4914696
Vishal Marati <106393542+vhack0679@users.noreply.github.com>
2025-10-05 17:47:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// [https://vitejs.dev/config/](https://vitejs.dev/config/)
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // This define block is the key to fixing the build error.
+  // It makes the environment variables available during the build process.
+  define: {
+    'import.meta.env.VITE_FIREBASE_CONFIG': `"${process.env.VITE_FIREBASE_CONFIG}"`
+  }
 })
